a022286731736d665fb0bf4c72c4750a1ad34a0e
Diego M. Rodrigues <diegomr86@gmail.com>
2020-04-22 13:49:14 -0300

    Atualiza script de deploy para usar a api agrofloresta-serve

--- publish_web.sh
+++ publish_web.sh
@@ -1,11 +1,10 @@
 rm -rf platforms/browser/
 ionic cordova build browser --prod --release
-rm -rf ../image-processor-server/www
-cp platforms/browser/www ../image-processor-server/ -rf
-cd ../image-processor-server
+rm -rf ../agrofloresta-server/static/*
+cp platforms/browser/www/* ../agrofloresta-server/static/ -rf
+cd ../agrofloresta-server
 git status
 git add .
-git commit -a -m 'Adiciona aplicação agrofloresta'
-git push dokku master
+git commit -a -m 'Adiciona build do app web agrofloresta para deploy'
 git push origin master
 cd ../agrofloresta
